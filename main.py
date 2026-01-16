d1e107569ae6da92d0cb4b49ea4534b644b7800a
Tony H. <tony.hirschligau@googlemail.com>
2026-01-16 10:28:18 +0100

    added write file with input content, clear entrys

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    try:
+        with open("data.txt", "a") as data:
+            data.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+            clear_entrys()
+    except FileNotFoundError:
+        with open("data.txt", "w") as data:
+            data.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+            clear_entrys()
 
+def clear_entrys():
+    website_entry.delete(0, "end")
+    password_entry.delete(0, "end")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -30,9 +42,11 @@ password_label.grid(column=0, row=3)
 # Entrys
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
+website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
+email_entry.insert(0, "tony.hirschligau@googlemail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, sticky="ew")
@@ -41,7 +55,7 @@ password_entry.grid(column=1, row=3, sticky="ew")
 generate_pw_button = Button(text="Generate Password")
 generate_pw_button.grid(column=2, row=3, sticky="w")
 
-add_pw_button = Button(text="Add", width=36)
+add_pw_button = Button(text="Add", width=36, command=save)
 add_pw_button.grid(column=1, row=4, columnspan=2, sticky="ew")
 
 
